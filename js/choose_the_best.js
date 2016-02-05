1c3f9121ceb8442d5b9d6a63e429d81d7c0e57ee
MessineseGianluca <gianluca.messi1@gmail.com>
2016-02-05 17:51:01 +0100

    Fix choose_the_best.js

--- js/choose_the_best.js
+++ js/choose_the_best.js
@@ -4,7 +4,7 @@ var smartphones = [
     {
       name: "Iphone 6s",
       ram: 2,
-      camera: 3,
+      camera: 8,
       price: 900
     },
     {
@@ -15,30 +15,35 @@ var smartphones = [
     },
     {
       name: "Asus Zenfone Selfie",
-      ram: 16,
-      camera: 27,
-      price: 499
+      ram: 2,
+      camera: 3,
+      price: 199
     }
 ];
 
+function chooseTheBest(ram, camera,  price) {
 
+    var preferences = [{ram: ram, camera: camera, price: price}];
+    var best_matching = [{name: "", score: 0}];
+    var score = 0;
 
-var preferences = [{ram: 10, camera: 2, price: 1}];
+    for(var i = 0; i < smartphones.length; i++){
+      score = 
+        (smartphones[i].ram * preferences[0].ram) +
+        (smartphones[i].camera * preferences[0].camera) + 
+        (preferences[0].price / smartphones[i].price);
+      
+      score = Math.abs(score);      
 
-var best_matching = [{name: "", score: 0}];
-
-var score = 0;
-for(var i = 0; i < smartphones.length; i++){
-  score = 
-    (smartphones[i].ram * preferences[0].ram) +
-    (smartphones[i].camera * preferences[0].camera) + 
-    (smartphones[i].price / preferences[0].price);
-
-  if(score > best_matching[0].score) { 
-    best_matching[0].name = smartphones[i].name;
-    best_matching[0].score = score;
-  }
+      if(score > best_matching[0].score) { 
+        best_matching[0].name = smartphones[i].name;
+        best_matching[0].score = score;
+      }
+    }
+    
+    return best_matching[0].name;
 }
 
-console.log(best_matching[0].name);
+console.log(chooseTheBest(5, 5, 10));
+
 
