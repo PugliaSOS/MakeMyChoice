8cada3df17374d23122ab14651f0582ea17f0381
Michele Rana <dev_mar@virgilio.it>
2016-02-08 18:04:36 +0100

    Update connect.js

--- js/connect.js
+++ js/connect.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 mongoose.connect('mongodb://host/database');
 
 // check connection
-if(mongoose.connection.readyState == 0){
+if(mongoose.connection.readyState != 1){
     console.log('failed connection');
     process.exit(1);
 } else {
