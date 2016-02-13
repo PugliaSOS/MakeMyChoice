69fd44596dfaee63f7c2f427ee9eb220bebf6b95
MessineseGianluca <gianluca.messi1@gmail.com>
2016-02-13 16:37:31 +0100

    Create api.js

---
+++ js/api/api.js
@@ -0,0 +1,66 @@
+// Create express app
+var app = require('express')();
+
+// Middleware to read POST's bodies
+app.use(require('body-parser').json());
+
+// GET / -> index.html
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
+
+
+/********************* Categories ********************/
+// GET /categories/smartphones  -> object with a category 
+app.get('/categories/:category', function(req, res) {
+    res.sendStatus(501);
+});
+
+// GET /categories -> an object with all categories
+app.get('/categories', function(req, res) {
+    res.sendStatus(501);
+});
+
+// POST /categories -> add a new category
+app.post('/categories', function(req, res) {
+    res.sendStatus(501);
+});
+
+// PUT /categories/smartphones -> modifies smartphone category
+app.put('/categories/:category', function(req, res) {
+    res.sendStatus(501);
+});
+
+/************************* Products *******************/
+
+// GET /categories/smartphones/products -> an object with all smartphones
+app.get('/categories/:category/products', function(req, res) {
+    res.sendStatus(501);
+});
+
+// GET /categories/smartphones/products/iphone -> an object with a smartphone 
+app.get('/categories/:category/products/:product', function(req, res) {
+    res.sendStatus(501);
+});
+
+// POST /categories/smartphones/products -> add a new product into a category
+app.post('/categories/:category/products', function(req, res) {
+    res.sendStatus(501);
+});
+
+// PUT /categories/smartphones/products/iphone -> modifies iphone characterstics
+app.put('/categories/:category/products/:product', function(req, res) {
+    res.sendStatus(501);
+});
+
+
+
+
+
+
+
+
+
+
+
+app.listen(8000);
