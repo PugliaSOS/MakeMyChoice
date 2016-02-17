d7a46aab3f2a1337acd047eee5a32dd88e1a218b
Michele Rana <dev_mar@virgilio.it>
2016-02-17 20:02:23 +0100

    Remove js folder, fix itemSchema, remove categorySchema

---
+++ models.js
@@ -0,0 +1,14 @@
+var mongoose = require('mongoose'); 
+
+var itemSchema = new mongoose.Schema({
+	title: String,
+	category: [String],
+	// attributes added dinamically
+	feeds : [mongoose.Schema.Types.Mixed] // array manage dinamically added attributes
+}, {strict: false});
+
+
+var Item = mongoose.model('Items', itemSchema);
+
+module.exports.itemSchema = itemSchema;
+module.exports.Item = Item;
