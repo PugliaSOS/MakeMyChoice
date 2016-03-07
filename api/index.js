51255b1a555def84cc65563c27d05933a60c49bb
Mario Lamacchia <mario@appstrakt.com>
2016-03-07 20:14:30 +0100

    Create start script and log port

--- api/index.js
+++ api/index.js
@@ -1,7 +1,9 @@
 var app = require('express')();
+var port = process.env.PORT || 8001;
 
 //it works only with /categories route
 app.use("/categories", require('./categories'));
 app.use("/categories", require('./products'));
 
-app.listen(8001);
+app.listen(port);
+console.log('app is listening on localhost:' + port);
