0a6ca58fb73fa5b3cbb10a291e2cb3faba01598d
MessineseGianluca <gianluca.messi1@gmail.com>
2016-03-10 01:13:20 +0100

    Start designing products page

--- api/index.js
+++ api/index.js
@@ -6,7 +6,7 @@ app.use(function(req, res, next) {
 	res.header('Access-Control-Allow-Origin', '*');
 	res.header('Access-Control-Allow-Credentials', true);
 	res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
-	res.header('Access-Control-Allow-Headers', 'Con8001tent-Type');
+	res.header('Access-Control-Allow-Headers', 'Content-Type');
 	next();
 });
 
